7eb93e94c4a10889c393ab6485029691245fd56b
Sam L <samlitowitz@users.noreply.github.com>
2018-10-04 09:27:04 -0400

    Add CCVResultCode constants

---
+++ src/Model/RapidConnect/Card/CCVResultCode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Omnipay\FirstData\Model\RapidConnect\Card;
+
+class CCVResultCode
+{
+    const MATCH = 'Match';
+    const NO_MATCH = 'NoMtch';
+    const NOT_PROCESSED = 'NotPrc';
+    const NOT_PROVIDED = 'NotPrv';
+    const NOT_PARTICIPATING = 'NotPrt';
+    const UNKNOWN = 'Unknwn';
+}
