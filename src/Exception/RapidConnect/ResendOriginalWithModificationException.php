a901aed5fdd402e68ebd6c35d0f59a5aeccd7492
Sam L <samlitowitz@users.noreply.github.com>
2018-10-11 10:07:24 -0400

    Add exceptions

---
+++ src/Exception/RapidConnect/ResendOriginalWithModificationException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Omnipay\FirstData\Exception\RapidConnect;
+
+use Omnipay\Common\Exception\OmnipayException;
+
+class ResendOriginalWithModificationException extends \Exception implements OmnipayException
+{
+}
