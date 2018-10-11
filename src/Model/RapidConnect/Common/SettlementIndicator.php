bf989b5bcd965ffef1f52f1d2446ae9b7193a717
Sam L <samlitowitz@users.noreply.github.com>
2018-10-11 10:01:23 -0400

    Add constants for settlement indicator

---
+++ src/Model/RapidConnect/Common/SettlementIndicator.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Omnipay\FirstData\Model\RapidConnect\Common;
+
+class SettlementIndicator
+{
+    const EXTERNAL = '1';
+    const ONLINE_TERMINAL_CAPTURE = '2';
+    const COMPASS_BATCH_SETTLEMENT = '3';
+}
